2eed89f3829e780b88ab6bae715057c7b12be831
andre <andre.willeke@googlemail.com>
2025-11-09 11:42:09 +0100

    refactoring

--- App/WebApp/src/middleware/notFound.js
+++ App/WebApp/src/middleware/notFound.js
@@ -1,5 +1,3 @@
-// const generateLayout = require("../utils/layout"); // NICHT MEHR BENÖTIGT
-
 /**
  * 404-Middleware (Catch-All-Handler).
  * Diese Funktion wird aufgerufen, wenn keine andere Express-Route
@@ -9,19 +7,11 @@
  * @param {function} next - Nächste Middleware-Funktion (wird hier nicht aufgerufen)
  */
 const notFoundHandler = (req, res, next) => {
-  // 1. Setzt den HTTP-Statuscode auf 404 (Not Found)
   res.status(404);
 
-  // 2. Rendert das EJS-Template "404". Express sucht dies in views/404.ejs
-  // Wir übergeben nur die benötigten spezifischen Daten.
   res.render("404", {
-    // Variable für den Seitentitel (wird im layout.ejs verwendet)
     title: req.__("404_NOT_FOUND_TITLE"),
-    // Variable, um die angeforderte URL im Template anzuzeigen (views/404.ejs)
     originalUrl: req.originalUrl,
-    // Die Styles für die 404-Seite sind jetzt im 404.ejs Template selbst eingebettet.
-    // Wir brauchen hier keine separate Style-Variable, da das EJS-Template
-    // den Inhalt und die Darstellung direkt liefert.
   });
 };
 
